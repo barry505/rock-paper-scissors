4276f404523d1973a0d0e1c0e7d3bbe614bbc745
Barry White <barry505@gmail.com>
2018-09-07 11:42:22 -0500

    added game function, it works

--- rps.js
+++ rps.js
@@ -10,6 +10,11 @@ function computerPlay() {
   }
 }
 
+function playerPlay() {
+  response = prompt('Enter Rock, Paper, Scissors:')
+  return response.toLowerCase();
+}
+
 function playRound(playerSelection, computerSelection) {
   if(playerSelection.toLowerCase() == computerSelection) {
     return 'Tie, play again.';
@@ -17,30 +22,48 @@ function playRound(playerSelection, computerSelection) {
   switch(playerSelection.toLowerCase()) {
     case 'rock':
       if(computerSelection == 'scissors') {
+        score++;
         return 'You Win! Rock beats Scissors';
       } else if (computerSelection == 'paper') {
+        score--;
         return 'You Lose! Paper beats Rock';
       }
       break;
     case 'paper':
       if(computerSelection == 'rock') {
+        score++;
         return 'You Win! Paper beats Rock';
       } else if(computerSelection == 'scissors') {
+        score--;
         return 'You Lose! Scissors beats Paper';
       }
       break;
     case 'scissors':
       if(computerSelection == 'paper') {
+        score++;
         return 'You Win! Scissors beats Paper';
       } else if(computerSelection == 'rock') {
+        score--;
         return 'You Lose! Rock beats Scissors';
       }
       break;
   }
 }
 
-const playerSelection = 'ROCK';
-const computerSelection = computerPlay();
-console.log(playerSelection.toLowerCase());
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  for(var i = 0; i < 5; i++) {
+    const playerSelection = playerPlay();
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+}
+
+let score = 0;
+game();
+if(score > 0) {
+  console.log('You Won! -- Score:', score);
+} else if(score < 0) {
+  console.log('You Lost! -- Score:', score);
+} else {
+  console.log('Tie game, try again.');
+}
