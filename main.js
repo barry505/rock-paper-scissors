85c4ae0fef6cc9c703ca9c3bbd11e497ca7a14de
Barry White <barry505@gmail.com>
2018-11-02 11:49:27 -0500

    Added DOM, minimal Bootstrap and CSS styling.

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+let computerScore = 0;
+let playerScore = 0;
+let res = document.querySelector('#result');
+let computer = document.querySelector('#computerScore');
+let player = document.querySelector('#playerScore');
+
+
 function computerPlay() {
   let num = Math.floor(Math.random() * 3) + 1;
-  switch(num) {
+  switch (num) {
     case 1:
       return 'rock';
     case 2:
@@ -10,40 +17,35 @@ function computerPlay() {
   }
 }
 
-function playerPlay() {
-  response = prompt('Enter Rock, Paper, Scissors:')
-  return response.toLowerCase();
-}
-
 function playRound(playerSelection, computerSelection) {
-  if(playerSelection.toLowerCase() == computerSelection) {
+  if (playerSelection == computerSelection) {
     return 'Tie, play again.';
   }
-  switch(playerSelection.toLowerCase()) {
+  switch (playerSelection) {
     case 'rock':
-      if(computerSelection == 'scissors') {
-        score++;
+      if (computerSelection == 'scissors') {
+        playerScore++;
         return 'You Win! Rock beats Scissors';
       } else if (computerSelection == 'paper') {
-        score--;
+        computerScore++;
         return 'You Lose! Paper beats Rock';
       }
       break;
     case 'paper':
-      if(computerSelection == 'rock') {
-        score++;
+      if (computerSelection == 'rock') {
+        playerScore++;
         return 'You Win! Paper beats Rock';
-      } else if(computerSelection == 'scissors') {
-        score--;
+      } else if (computerSelection == 'scissors') {
+        computerScore++;
         return 'You Lose! Scissors beats Paper';
       }
       break;
     case 'scissors':
-      if(computerSelection == 'paper') {
-        score++;
+      if (computerSelection == 'paper') {
+        playerScore++;
         return 'You Win! Scissors beats Paper';
-      } else if(computerSelection == 'rock') {
-        score--;
+      } else if (computerSelection == 'rock') {
+        computerScore++;
         return 'You Lose! Rock beats Scissors';
       }
       break;
@@ -51,19 +53,26 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-  for(var i = 0; i < 5; i++) {
-    const playerSelection = playerPlay();
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-  }
+
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      const computerSelection = computerPlay();
+      res.textContent = playRound(button.id, computerSelection);
+      player.textContent = "Player Score: " + playerScore;
+      computer.textContent = "Computer Score: " + computerScore;
+      if (playerScore == 5) {
+        res.textContent = "Player Wins!";
+        computerScore = 0;
+        playerScore = 0;
+      }
+      if (computerScore == 5) {
+        res.textContent = "Computer Wins";
+        computerScore = 0;
+        playerScore = 0;
+      }
+    })
+  })
 }
 
-let score = 0;
 game();
-if(score > 0) {
-  console.log('You Won! -- Score:', score);
-} else if(score < 0) {
-  console.log('You Lost! -- Score:', score);
-} else {
-  console.log('Tie game, try again.');
-}
