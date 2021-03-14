fe9b55633c78ed547bb5d02f9da68ba21e919244
Barry White <barry505@gmail.com>
2021-03-14 09:17:56 -0500

    started playRound() function

--- main-no-ui.js
+++ main-no-ui.js
@@ -8,7 +8,7 @@
 
 function computerPlay() {
   let play = Math.floor((Math.random() * 3) + 1);
-  switch(play) {
+  switch (play) {
     case 1:
       return 'Rock';
       break;
@@ -19,4 +19,27 @@ function computerPlay() {
       return 'Scissors';
       break;
   }
+}
+
+/* Write a function that plays a single round of Rock Paper Scissors.
+   The function should take two parameters - the playerSelection
+   and computerSelection - and then return a string that declares
+   the winner of the round like so: "You Lose! Paper beats Rock"
+   1. Make you function case insensitive (so users can input rock,
+    ROCK, Rock or any other variation).
+
+    Important note: you want to return the results of this function
+    call, not console.log() them. To test this function console.log
+    the results:
+
+    function playRound(playerSelection, computerSelection) {
+      // your code here!
+    }
+
+    const playerSelection = "rock";
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection)); */
+
+function playRound(playerSelection, computerSelection) {
+
 }
